2cd2c72312b3d644e89d8f4f61358de566cfdd28
ParthivSen <74867166+ParthivSen@users.noreply.github.com>
2021-10-06 22:19:11 +0530

    Pattern

---
+++ Pattern(1).py
@@ -0,0 +1,5 @@
+s='#'
+p=''
+for a in range(0,5):
+    p+=s
+    print(p)
---
+++ Pattern(3).py
@@ -0,0 +1,10 @@
+# 6 5 4 3 2 1
+# 5 4 3 2 1
+# 4 3 2 1
+# 3 2 1
+# 2 1
+# 1
+for a in range(5,-1,-1):
+    for b in range(a+1,0,-1):
+        print(b,end='')
+    print()    
